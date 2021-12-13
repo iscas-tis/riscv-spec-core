c7df698ec6fc1dff3ccd93caedb9b806c5485369
liuyic00 <liuyic00@gmail.com>
2021-12-13 11:39:57 +0800

    test: add ADDI test in RiscvCoreSpec

--- src/test/scala/rvspeccore/core/RiscvCoreSpec.scala
+++ src/test/scala/rvspeccore/core/RiscvCoreSpec.scala
@@ -4,11 +4,23 @@ import chisel3._
 import chiseltest._
 import org.scalatest._
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
 
-class RiscvCoreSpec extends AnyFlatSpec with ChiselScalatestTester with Matchers {
+class RiscvCoreSpec extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "RiscvCore"
-  it should "pass" in {
-    test(new RiscvCore) { c => }
+  it should "pass ADDI test" in {
+    test(new RiscvCore) { c =>
+      c.io.inst.poke("b0000_0000_0001__00001__000__00010__0010011".U(32.W)) // ADDI R2 = R1 + 1
+      c.io.valid.poke(true.B)
+
+      c.io.now.reg(1).expect(0.U)
+      c.io.now.reg(2).expect(0.U)
+      c.io.next.reg(1).expect(0.U)
+      c.io.next.reg(2).expect(1.U)
+      c.clock.step()
+      c.io.now.reg(1).expect(0.U)
+      c.io.now.reg(2).expect(1.U)
+      c.io.next.reg(1).expect(0.U)
+      c.io.next.reg(2).expect(1.U)
+    }
   }
 }
