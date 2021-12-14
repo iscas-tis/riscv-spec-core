3fc3dbb0a893b6d40d6a4bb1cbbb1e443b830521
liuyic00 <liuyic00@gmail.com>
2021-12-14 20:36:22 +0800

    feat: add CheckerWithWB

--- src/main/scala/rvspeccore/checker/Checker.scala
+++ src/main/scala/rvspeccore/checker/Checker.scala
@@ -7,6 +7,12 @@ import rvspeccore.core._
 
 abstract class Checker extends Module
 
+/** Checker with state port.
+  *
+  * Link to the `CheckerCore` directly.
+  *
+  * @param gen
+  */
 class CheckerWithState[T <: RiscvCore](gen: => T) extends Checker {
   val io = IO(new Bundle {
     val inst  = Input(UInt(32.W))
@@ -18,4 +24,41 @@ class CheckerWithState[T <: RiscvCore](gen: => T) extends Checker {
   // link to checkerCore
   val checkerCore = Module(new CheckerCore(gen))
   checkerCore.io <> io
+}
+
+class WriteBack extends Bundle {
+  val valid = Bool()
+  val dest  = UInt(8.W)
+  val data  = UInt(64.W)
+}
+
+/** Checker with write back port.
+  *
+  * Write back will work when `io.wb.valid` is true, But only check assert after
+  * commit instruction with `io.valid`.
+  *
+  * @param gen
+  *   Generator of a spec core
+  */
+class CheckerWithWB[T <: RiscvCore](gen: => T) extends Checker {
+  val io = IO(new Bundle {
+    val inst  = Input(UInt(32.W))
+    val valid = Input(Bool())
+
+    val pc = Input(UInt(64.W))
+    val wb = Input(new WriteBack)
+  })
+
+  val reg = RegInit(VecInit(Seq.fill(32)(0.U(64.W))))
+
+  when(io.wb.valid) {
+    reg(io.wb.dest) := io.wb.data
+  }
+
+  // link to checkerCore
+  val checkerCore = Module(new CheckerCore(gen))
+  checkerCore.io.inst      := io.inst
+  checkerCore.io.valid     := io.valid
+  checkerCore.io.state.reg := reg
+  checkerCore.io.state.pc  := io.pc
 }
--- src/test/scala/rvspeccore/checker/CheckerSpec.scala
+++ src/test/scala/rvspeccore/checker/CheckerSpec.scala
@@ -20,3 +20,30 @@ class CheckerWithStateSpec extends AnyFlatSpec with Formal with ChiselScalatestT
     verify(new TestCore, Seq(BoundedCheck(3)))
   }
 }
+
+class CheckerWithWBSpec extends AnyFlatSpec with Formal with ChiselScalatestTester {
+  behavior of "CheckerWithWB"
+  it should "pass" in {
+    class TestCore extends RiscvCore {
+      val wb = Wire(new WriteBack)
+      wb.valid := false.B
+      wb.dest  := 0.U
+      wb.data  := 0.U
+
+      for (i <- 0 until 32) {
+        when(now.reg(i.U) =/= next.reg(i.U)) {
+          wb.valid := true.B
+          wb.dest  := i.U
+          wb.data  := next.reg(i.U)
+        }
+      }
+
+      val checker = Module(new CheckerWithWB(new RiscvCore))
+      checker.io.inst  := io.inst
+      checker.io.valid := io.valid
+      checker.io.pc    := now.pc
+      checker.io.wb    := wb
+    }
+    verify(new TestCore, Seq(BoundedCheck(3)))
+  }
+}
