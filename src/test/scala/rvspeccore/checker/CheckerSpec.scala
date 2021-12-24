404867c1a1823846a5e0d26f25584e7e126b0a9f
liuyic00 <liuyic00@gmail.com>
2021-12-24 20:53:46 +0800

    test: remove formal verify

--- src/test/scala/rvspeccore/checker/CheckerSpec.scala
+++ src/test/scala/rvspeccore/checker/CheckerSpec.scala
@@ -2,28 +2,29 @@ package rvspeccore.checker
 
 import chisel3._
 import chiseltest._
-import chiseltest.formal._
 import org.scalatest._
 import org.scalatest.flatspec.AnyFlatSpec
 
 import rvspeccore.core._
 
-class CheckerWithStateSpec extends AnyFlatSpec with Formal with ChiselScalatestTester {
+class CheckerWithStateSpec extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "CheckerWithState"
-  it should "pass" in {
+  it should "pass RiscvCoreTest" in {
     class TestCore extends RiscvCore {
       val checker = Module(new CheckerWithState(new RiscvCore))
       checker.io.inst  := io.inst
       checker.io.valid := io.valid
       checker.io.state := now
     }
-    verify(new TestCore, Seq(BoundedCheck(3)))
+    test(new TestCore) { c =>
+      RiscvCoreTest(c)
+    }
   }
 }
 
-class CheckerWithWBSpec extends AnyFlatSpec with Formal with ChiselScalatestTester {
+class CheckerWithWBSpec extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "CheckerWithWB"
-  it should "pass" in {
+  it should "pass RiscvCoreTest" in {
     class TestCore extends RiscvCore {
       val wb = Wire(new WriteBack)
       wb.valid := false.B
@@ -44,6 +45,8 @@ class CheckerWithWBSpec extends AnyFlatSpec with Formal with ChiselScalatestTest
       checker.io.pc    := now.pc
       checker.io.wb    := wb
     }
-    verify(new TestCore, Seq(BoundedCheck(3)))
+    test(new TestCore) { c =>
+      RiscvCoreTest(c)
+    }
   }
 }
--- src/test/scala/rvspeccore/core/RiscvCoreSpec.scala
+++ src/test/scala/rvspeccore/core/RiscvCoreSpec.scala
@@ -5,24 +5,44 @@ import chiseltest._
 import org.scalatest._
 import org.scalatest.flatspec.AnyFlatSpec
 
+object RiscvCoreTest {
+  def addi(c: RiscvCore): Unit = {
+    c.reset.poke(true.B)
+    c.clock.step()
+
+    c.reset.poke(false.B)
+    c.io.valid.poke(true.B)
+    c.io.inst.poke("b0000_0000_0001__00001__000__00010__0010011".U(32.W)) // ADDI R2 = R1 + 1
+    c.io.now.reg(1).expect(0.U)
+    c.io.now.reg(2).expect(0.U)
+    c.io.next.reg(1).expect(0.U)
+    c.io.next.reg(2).expect(1.U)
+    c.clock.step()
+
+    c.io.now.reg(1).expect(0.U)
+    c.io.now.reg(2).expect(1.U)
+    c.io.next.reg(1).expect(0.U)
+    c.io.next.reg(2).expect(1.U)
+    c.clock.step()
+  }
+
+  val tests = Seq(
+    addi(_),
+    addi(_)
+  )
+
+  def apply(c: RiscvCore): Unit = {
+    tests.foreach(_(c))
+  }
+}
+
 class RiscvCoreSpec extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "RiscvCore"
-  it should "pass ADDI test" in {
+  it should "pass RiscvCoreTest" in {
     (new chisel3.stage.ChiselStage)
       .emitFirrtl(new RiscvCore, Array("--target-dir", "test_run_dir/" + getTestName))
     test(new RiscvCore) { c =>
-      c.io.inst.poke("b0000_0000_0001__00001__000__00010__0010011".U(32.W)) // ADDI R2 = R1 + 1
-      c.io.valid.poke(true.B)
-
-      c.io.now.reg(1).expect(0.U)
-      c.io.now.reg(2).expect(0.U)
-      c.io.next.reg(1).expect(0.U)
-      c.io.next.reg(2).expect(1.U)
-      c.clock.step()
-      c.io.now.reg(1).expect(0.U)
-      c.io.now.reg(2).expect(1.U)
-      c.io.next.reg(1).expect(0.U)
-      c.io.next.reg(2).expect(1.U)
+      RiscvCoreTest(c)
     }
   }
 }
