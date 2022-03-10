d894cd54c900f4696252d51399cf52be567cb082
liuyic00 <liuyic00@gmail.com>
2022-03-10 22:09:17 +0800

    feat: add AssumeHelper

---
+++ src/main/scala/rvspeccore/checker/AssumeHelper.scala
@@ -0,0 +1,84 @@
+package rvspeccore.checker
+
+import chisel3._
+import chisel3.util._
+
+import rvspeccore.core.spec
+
+abstract class AssumeHelper {
+  val partition: Seq[AssumeHelper]
+  lazy val list32: Seq[spec.Inst]   = partition.map(_.list32).flatten
+  lazy val append64: Seq[spec.Inst] = partition.map(_.append64).flatten
+
+  def size(implicit XLEN: Int): Int = {
+    require(XLEN == 32 || XLEN == 64)
+    XLEN match {
+      case 32 => list32.size
+      case 64 => (list32 ++ append64).size
+    }
+  }
+
+  def apply(inst: UInt)(implicit XLEN: Int): Bool = {
+    require(XLEN == 32 || XLEN == 64)
+    XLEN match {
+      case 32 => list32.map(_(inst)(XLEN)).reduce(_ || _)
+      case 64 => (list32 ++ append64).map(_(inst)(XLEN)).reduce(_ || _)
+    }
+  }
+}
+object AssumeHelper {
+  def apply(list32A: Seq[spec.Inst], append64A: Seq[spec.Inst]): AssumeHelper = {
+    new AssumeHelper {
+      val partition              = List()
+      override lazy val list32   = list32A
+      override lazy val append64 = append64A
+    }
+  }
+  def apply(list32A: Seq[spec.Inst]): AssumeHelper = {
+    new AssumeHelper {
+      val partition              = List()
+      override lazy val list32   = list32A
+      override lazy val append64 = List()
+    }
+  }
+}
+
+object RV extends AssumeHelper with spec.RVInsts {
+  val partition: Seq[AssumeHelper] = List(RVG)
+}
+object RVG extends AssumeHelper with spec.RVG {
+  val partition = List(RVI, RVM)
+}
+object RVI extends AssumeHelper with spec.instset.RVI {
+  val regImm = AssumeHelper(
+    List(ADDI,  SLTI,  SLTIU, ADDI,  ORI, XORI, SLLI, SRLI, SRAI, LUI, AUIPC),
+    List(ADDIW, SLLIW, SRLIW, SRLIW, SRAIW)
+  )
+  val regReg = AssumeHelper(
+    List(ADD, SLT, SLTU, AND, OR, XOR, SLL, SRL, SUB, SRA)
+  )
+  val control = AssumeHelper(
+    List(JAL, JALR, BEQ, BNE, BLT, BLTU, BGE, BGEU)
+  )
+  val loadStore = AssumeHelper(
+    List(LB,  LH, LW, LBU, LHU, SB, SH, SW),
+    List(LWU, LD, SD)
+  )
+  val other = AssumeHelper(
+    List(FENCE, ECALL, EBREAK)
+  )
+
+  val partition = List(regImm, regReg, control, loadStore, other)
+}
+object RVM extends AssumeHelper with spec.instset.RVM {
+  val mulOp = AssumeHelper(
+    List(MUL, MULH, MULHSU, MULHU),
+    List(MULW)
+  )
+  val divOp = AssumeHelper(
+    List(DIV,  DIVU,  REM,  REMU),
+    List(DIVW, DIVUW, REMW, REMUW)
+  )
+
+  val partition: Seq[AssumeHelper] = List(mulOp, divOp)
+}
---
+++ src/test/scala/rvspeccore/checker/AssumeHelperSpec.scala
@@ -0,0 +1,33 @@
+package rvspeccore.checker
+
+import chisel3._
+import chiseltest._
+import org.scalatest._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class AssumeHelperSpec extends AnyFlatSpec with ChiselScalatestTester {
+  behavior of "AssumeHelper"
+  it should "pass test" in {
+    class testDUT(instCheck: (UInt) => Bool, inst: UInt) extends Module {
+      val out = IO(Output(Bool()))
+      out := instCheck(inst)
+    }
+
+    test(new testDUT(RVI.LUI(_)(64), "b0000_0000_0000_0000_0000__00000__0110111".U(32.W))) { c =>
+      c.out.expect(true.B)
+    }
+    test(new testDUT(RVG.ADDI(_)(32), "b0000_0000_0000__00000_000_00000__0010011".U(32.W))) { c =>
+      c.out.expect(true.B)
+    }
+    test(new testDUT(RV.SLLI(_)(64), "b0000000__00000__00000_001_00000__0010011".U(32.W))) { c =>
+      c.out.expect(true.B)
+    }
+    test(new testDUT(RV(_)(64), "b0000000__00000__00000_001_00000__0010011".U(32.W))) { c =>
+      c.out.expect(true.B)
+    }
+    // false
+    test(new testDUT(RV.ADDI(_)(64), "b0000_0000_0000__00000_001_00000__0010011".U(32.W))) { c =>
+      c.out.expect(false.B)
+    }
+  }
+}
