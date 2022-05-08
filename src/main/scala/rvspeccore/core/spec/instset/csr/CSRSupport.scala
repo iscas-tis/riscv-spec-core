78595cdaf316d124266bf52d452632d14103a6ea
liuyic00 <liuyic00@gmail.com>
2022-05-08 14:15:16 +0800

    feat: add MstatusStruct

--- src/main/scala/rvspeccore/core/spec/instset/csr/CSRSupport.scala
+++ src/main/scala/rvspeccore/core/spec/instset/csr/CSRSupport.scala
@@ -66,3 +66,57 @@ trait CSRSupport extends BaseCore {
     // ...
   }
 }
+
+/** unpack mstatus with this Bundle
+  *
+  * <https://www.chisel-lang.org/chisel3/docs/cookbooks/cookbook#how-do-i-unpack-a-value-reverse-concatenation-like-in-verilog>
+  *
+  *   - riscv-privileged-20211203
+  *   - Chapter 3: Machine-Level ISA, Version 1.12
+  *   - 3.1 Machine-Level CSRs
+  *   - 3.1.6 Machine Status Registers (mstatus and mstatush)
+  *     - Figure 3.6: Machine-mode status register (mstatus) for RV32.
+  *     - Figure 3.7: Machine-mode status register (mstatus) for RV64.
+  */
+class MstatusStruct()(implicit XLEN: Int) extends Bundle {
+  // common
+  // 31 or 63
+  val SD = UInt(1.W)
+
+  // RV32
+  // 30 - 23
+  val WPRI_30_23 = if (XLEN == 32) UInt(8.W) else null
+
+  // RV64
+  // 62 - 23
+  val WPRI_62_38 = if (XLEN == 64) UInt(25.W) else null
+  val MBE        = if (XLEN == 64) UInt(2.W) else null
+  val SBE        = if (XLEN == 64) UInt(2.W) else null
+  val SXL        = if (XLEN == 64) UInt(2.W) else null
+  val UXL        = if (XLEN == 64) UInt(2.W) else null
+  val WPRI_31_23 = if (XLEN == 64) UInt(9.W) else null
+
+  // common
+  // 22 - 17
+  val TSR  = UInt(1.W)
+  val TW   = UInt(1.W)
+  val TVM  = UInt(1.W)
+  val MXR  = UInt(1.W)
+  val SUM  = UInt(1.W)
+  val MPRV = UInt(1.W)
+  // 16 - 9
+  val XS  = UInt(2.W)
+  val FS  = UInt(2.W)
+  val MPP = UInt(2.W)
+  val VS  = UInt(2.W)
+  // 8 - 0
+  val SPP    = UInt(1.W)
+  val MPIE   = UInt(1.W)
+  val UBE    = UInt(1.W)
+  val SPIE   = UInt(1.W)
+  val WPRI_4 = UInt(1.W)
+  val MIE    = UInt(1.W)
+  val WPRI_2 = UInt(1.W)
+  val SIE    = UInt(1.W)
+  val WPRI_0 = UInt(1.W)
+}
