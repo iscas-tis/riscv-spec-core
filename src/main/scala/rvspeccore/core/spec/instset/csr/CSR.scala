a196efc6eb876df4a11129753ce3e33aa12c7c5b
SeddonShen <283481855@qq.com>
2022-11-10 17:10:00 +0800

    CSR Init

--- src/main/scala/rvspeccore/core/spec/instset/csr/CSR.scala
+++ src/main/scala/rvspeccore/core/spec/instset/csr/CSR.scala
@@ -38,14 +38,15 @@ object CSRInfo {
   */
 object CSRInfos {
   // - User Trap Setup ???????????
-  val ustatus  = CSRInfo("h000") // TODO
-  val utvec    = CSRInfo("h005") // TODO
-  val uip      = CSRInfo("h044") // TODO
-  val uie      = CSRInfo("h004") // TODO
-  val uscratch = CSRInfo("h040") // TODO
-  val uepc     = CSRInfo("h041") // TODO
-  val ucause   = CSRInfo("h042") // TODO
-  val utval    = CSRInfo("h043") // TODO
+  // User CSR has been delete in V20211203
+  // val ustatus  = CSRInfo("h000") // TODO
+  // val utvec    = CSRInfo("h005") // TODO
+  // val uip      = CSRInfo("h044") // TODO
+  // val uie      = CSRInfo("h004") // TODO
+  // val uscratch = CSRInfo("h040") // TODO
+  // val uepc     = CSRInfo("h041") // TODO
+  // val ucause   = CSRInfo("h042") // TODO
+  // val utval    = CSRInfo("h043") // TODO
 
   // - Unprivileged Floating-Point CSRs
   // - Unprivileged Counter/Timers
@@ -76,10 +77,10 @@ object CSRInfos {
   // - Virtual Supervisor Registers
 
   // - Machine Information Registers
-  val mvendorid = CSRInfo("hf11") // TODO
-  val marchid   = CSRInfo("hf12") // TODO
-  val mimpid    = CSRInfo("hf13") // TODO
-  val mhartid   = CSRInfo("hf14") // TODO
+  val mvendorid = CSRInfo("hf11", None, false) // TODO
+  val marchid   = CSRInfo("hf12", None, false) // TODO
+  val mimpid    = CSRInfo("hf13", None, false) // TODO: not mention whether can write or not
+  val mhartid   = CSRInfo("hf14", None, false) // TODO
   // mconfigptr
   // - Machine Information Registers
   val mstatus    = CSRInfo("h300") // TODO
@@ -87,8 +88,9 @@ object CSRInfos {
   val medeleg    = CSRInfo("h302") // TODO
   val mideleg    = CSRInfo("h303") // TODO
   val mie        = CSRInfo("h304") // TODO
-  val mtvec      = CSRInfo("h305")
+  val mtvec      = CSRInfo("h305") // TODO
   val mcounteren = CSRInfo("h306") // TODO
+  val mstatush   = CSRInfo("h310") // TODO
   // mstatush
   // - Machine Trap Handling
   val mscratch = CSRInfo("h340") // TODO
@@ -96,6 +98,25 @@ object CSRInfos {
   val mcause   = CSRInfo("h342") // TODO
   val mtval    = CSRInfo("h343")
   val mip      = CSRInfo("h344") // TODO
+  val mtinst   = CSRInfo("h34A")
+  val mtval2   = CSRInfo("h34B")
+
+  // Machine Configuration
+  val menvcfg  = CSRInfo("h30A")
+  val menvcfgh = CSRInfo("h31A")
+  val mseccfg  = CSRInfo("h747")
+  val mseccfgh = CSRInfo("h757")
+  // ...
+  // Machine Counter/Timers
+  val mcycle        = CSRInfo("hb00")
+  val minstret      = CSRInfo("hb02")
+  val mhpmcounter3  = CSRInfo("hb03")
+  val mhpmcounter4  = CSRInfo("hb04")
+  // .. TODO: for mhpmcounter3 ~ mhpmcounter31
+  val mcycleh       = CSRInfo("hb80")
+  val mhpmcounter3h = CSRInfo("hb82")
+  // .. TODO: for mhpmcounter3h ~ mhpmcounter31h
+
   // mtinst
   // mtval2
   // - Machine Trap Handling
@@ -107,6 +128,10 @@ case class CSRInfoSignal(info: CSRInfo, signal: UInt)
 
 class CSR()(implicit XLEN: Int) extends Bundle with IgnoreSeqInBundle {
   val misa   = CSRInfos.misa.makeUInt
+  val mvendorid = CSRInfos.mvendorid.makeUInt
+  val marchid = CSRInfos.marchid.makeUInt
+  val mimpid = CSRInfos.mimpid.makeUInt
+  val mhartid = CSRInfos.mhartid.makeUInt
   val mtvec  = CSRInfos.mtvec.makeUInt
   val mepc   = CSRInfos.mepc.makeUInt
   val mcause = CSRInfos.mcause.makeUInt
@@ -140,7 +165,15 @@ object CSR {
   def apply()(implicit XLEN: Int): CSR = new CSR
   def wireInit()(implicit XLEN: Int, config: RVConfig): CSR = {
     val csr = Wire(new CSR)
+    // TODO: same with data RVConfig
     csr.misa   := 0.U
+    // mvendorid value 0 means non-commercial implementation
+    csr.mvendorid := 0.U
+    // marchid allocated globally by RISC-V International 0 means not implementation
+    csr.marchid := 0.U
+    // mimpid 0 means not implementation
+    csr.mimpid := 0.U
+    csr.mhartid := 0.U
     csr.mtvec  := 0.U
     csr.mepc   := 0.U
     csr.mcause := 0.U
