216133d6e04f3a36847765b50ca2b3a9cbba027d
SeddonShen <283481855@qq.com>
2024-11-04 15:14:29 +0800

    style: change isa comments for zba, zbb, zbc, zbs, zbkb, zbkc and zbkx.

--- src/main/scala/rvspeccore/core/spec/instset/BExtension.scala
+++ src/main/scala/rvspeccore/core/spec/instset/BExtension.scala
@@ -7,32 +7,31 @@ import rvspeccore.core.BaseCore
 import rvspeccore.core.spec._
 import rvspeccore.core.tool.BitTool._
 
-// scalafmt: { maxColumn = 200 }
 /** "B" Extension for Bit Manipulation, Version 1.0.0
   *
   *   - riscv-spec-20240411
   *   - Chapter 28: "B" Extension for Bit Manipulation, Version 1.0.0
-  *   - The B standard extension comprises instructions provided by the Zba, Zbb, and Zbs extensions.
+  *   - 28.5. Instructions (in alphabetical order)
   */
 trait BExtensionInsts {
   val add_uw = Inst("b0000100_?????_?????_000_?????_0111011")
   val andn   = Inst("b0100000_?????_?????_111_?????_0110011")
   val bclr   = Inst("b0100100_?????_?????_001_?????_0110011")
-  val bclri  = Inst(
+  val bclri = Inst(
     32 -> "b0100100_?????_?????_011_?????_0110011",
     64 -> "b010010_??????_?????_011_?????_0110011"
   )
-  val bext   = Inst("b0100100_?????_?????_101_?????_0110011")
-  val bexti  = Inst(
+  val bext = Inst("b0100100_?????_?????_101_?????_0110011")
+  val bexti = Inst(
     32 -> "b0100100_?????_?????_101_?????_0010011",
     64 -> "b010010_??????_?????_101_?????_0010011"
   )
-  val binv   = Inst("b0110100_?????_?????_001_?????_0110011")
-  val binvi  = Inst(
+  val binv = Inst("b0110100_?????_?????_001_?????_0110011")
+  val binvi = Inst(
     32 -> "b0110100_?????_?????_001_?????_0010011",
     64 -> "b011010_??????_?????_001_?????_0010011"
   )
-  val bseti  = Inst(
+  val bseti = Inst(
     32 -> "b0010100_?????_?????_001_?????_0010011",
     64 -> "b001010_??????_?????_001_?????_0010011"
   )
@@ -55,70 +54,73 @@ trait BExtensionInsts {
   val pack   = Inst("b0000100_?????_?????_100_?????_0110011")
   val packh  = Inst("b0000100_?????_?????_111_?????_0110011")
   val packw  = Inst("b0000100_?????_?????_100_?????_0111011")
-  val rev8   = Inst(
+  val rev8 = Inst(
     32 -> "b011010011000_?????_101_?????_0010011",
     64 -> "b011010111000_?????_101_?????_0010011"
   )
-  val rev_b  = Inst("b011010000111_?????_101_?????_0010011")
-  val rol    = Inst("b0110000_?????_?????_001_?????_0110011")
-  val rolw   = Inst("b0110000_?????_?????_001_?????_0111011")
-  val ror    = Inst("b0110000_?????_?????_101_?????_0110011")
-  val rori   = Inst(
+  val rev_b = Inst("b011010000111_?????_101_?????_0010011")
+  val rol   = Inst("b0110000_?????_?????_001_?????_0110011")
+  val rolw  = Inst("b0110000_?????_?????_001_?????_0111011")
+  val ror   = Inst("b0110000_?????_?????_101_?????_0110011")
+  val rori = Inst(
     32 -> "b0110000_?????_?????_101_?????_0010011",
     64 -> "b011000_??????_?????_101_?????_0010011"
   )
-  val roriw  = Inst("b0110000_?????_?????_101_?????_0011011")
-  val rorw   = Inst("b0110000_?????_?????_101_?????_0111011")
-  val sext_b = Inst("b0110000_00100_?????_001_?????_0010011")
-  val sext_h = Inst("b0110000_00101_?????_001_?????_0010011")
-  val sh1add = Inst("b0010000_?????_?????_010_?????_0110011")
+  val roriw     = Inst("b0110000_?????_?????_101_?????_0011011")
+  val rorw      = Inst("b0110000_?????_?????_101_?????_0111011")
+  val sext_b    = Inst("b0110000_00100_?????_001_?????_0010011")
+  val sext_h    = Inst("b0110000_00101_?????_001_?????_0010011")
+  val sh1add    = Inst("b0010000_?????_?????_010_?????_0110011")
   val sh1add_uw = Inst("b0010000_?????_?????_010_?????_0111011")
-  val sh2add = Inst("b0010000_?????_?????_100_?????_0110011")
+  val sh2add    = Inst("b0010000_?????_?????_100_?????_0110011")
   val sh2add_uw = Inst("b0010000_?????_?????_100_?????_0111011")
-  val sh3add = Inst("b0010000_?????_?????_110_?????_0110011")
+  val sh3add    = Inst("b0010000_?????_?????_110_?????_0110011")
   val sh3add_uw = Inst("b0010000_?????_?????_110_?????_0111011")
-  val slli_uw = Inst("b000010_??????_?????_001_?????_0011011")
-  val unzip  = Inst("b0000100_11111_?????_101_?????_0010011")
-  val xnor   = Inst("b0100000_?????_?????_100_?????_0110011")
-  val xperm_b= Inst("b0010100_?????_?????_100_?????_0110011")
-  val xperm_n= Inst("b0010100_?????_?????_010_?????_0110011")
+  val slli_uw   = Inst("b000010_??????_?????_001_?????_0011011")
+  val unzip     = Inst("b0000100_11111_?????_101_?????_0010011")
+  val xnor      = Inst("b0100000_?????_?????_100_?????_0110011")
+  val xperm_b   = Inst("b0010100_?????_?????_100_?????_0110011")
+  val xperm_n   = Inst("b0010100_?????_?????_010_?????_0110011")
   val zext_h = Inst(
     32 -> "b0000100_00000_?????_100_?????_0110011",
     64 -> "b0000100_00000_?????_100_?????_0111011"
   )
-  val zip    = Inst("b0000100_11110_?????_001_?????_0010011")
+  val zip = Inst("b0000100_11110_?????_001_?????_0010011")
 }
 
 /** "B" Extension for Bit Manipulation, Version 1.0.0
   *
   *   - riscv-spec-20240411
   *   - Chapter 28: "B" Extension for Bit Manipulation, Version 1.0.0
+  *   - 28.4. Extensions
   */
 trait BExtension extends BaseCore with CommonDecode with BExtensionInsts {
+
   /** Address generation
-   * The Zba instructions can be used to accelerate the generation of addresses that index into arrays of
-   * basic types (halfword, word, doubleword) using both unsigned word-sized and XLEN-sized indices: a
-   * shifted index is added to a base address.
-   * - riscv-spec-20240411 P212
-   * - Chapter 28.4.1
-   */
+    *
+    *   - riscv-spec-20240411 P212
+    *   - 28.4.1. Zba: Address generation
+    */
   def doRV32Zba: Unit = {
-    when(sh1add(inst)) {decodeR; next.reg(rd) := now.reg(rs2) + (now.reg(rs1) << 1)}
+    when(sh1add(inst)) { decodeR; next.reg(rd) := now.reg(rs2) + (now.reg(rs1) << 1) }
     when(sh2add(inst)) { decodeR; next.reg(rd) := now.reg(rs2) + (now.reg(rs1) << 2) }
     when(sh3add(inst)) { decodeR; next.reg(rd) := now.reg(rs2) + (now.reg(rs1) << 3) }
   }
+
   /** Basic bit-manipulation
-   * - riscv-spec-20240411 P212
-   * - Chapter 28.4.2
-   */
+    *
+    *   - riscv-spec-20240411 P212
+    *   - 28.4.2. Zbb: Basic bit-manipulation
+    */
   def doRV32Zbb: Unit = {
+    // scalafmt: { maxColumn = 200 }
     // Logical with negate
     when(andn(inst)) { decodeR; next.reg(rd) := now.reg(rs1) & (~now.reg(rs2)).asUInt }
-    when(orn(inst)) { decodeR; next.reg(rd) := now.reg(rs1) | (~now.reg(rs2)).asUInt }
+    when(orn(inst))  { decodeR; next.reg(rd) := now.reg(rs1) | (~now.reg(rs2)).asUInt }
     when(xnor(inst)) { decodeR; next.reg(rd) := (~(now.reg(rs1) ^ now.reg(rs2))).asUInt }
     // Count leading/trailing zero bits
-    when(clz(inst)) { decodeI; next.reg(rd) := Mux(now.reg(rs1) === 0.U, XLEN.U, PriorityEncoder(now.reg(rs1).asBools.reverse))}
-    when(ctz(inst)) { decodeI; next.reg(rd) := Mux(now.reg(rs1) === 0.U, XLEN.U, PriorityEncoder(now.reg(rs1).asBools))}
+    when(clz(inst)) { decodeI; next.reg(rd) := Mux(now.reg(rs1) === 0.U, XLEN.U, PriorityEncoder(now.reg(rs1).asBools.reverse)) }
+    when(ctz(inst)) { decodeI; next.reg(rd) := Mux(now.reg(rs1) === 0.U, XLEN.U, PriorityEncoder(now.reg(rs1).asBools)) }
     // Count population
     when(cpop(inst)) { decodeI; next.reg(rd) := PopCount(now.reg(rs1)) }
     // Integer minimum/maximum
@@ -135,16 +137,16 @@ trait BExtension extends BaseCore with CommonDecode with BExtensionInsts {
     def getRotationShamt(value: UInt, xlen: Int): UInt = {
       value(if (xlen == 32) 4 else 5, 0)
     }
-    when(rol(inst)) { decodeR; next.reg(rd) := (now.reg(rs1) << getRotationShamt(now.reg(rs2), XLEN)).asUInt | (now.reg(rs1) >> (XLEN.U - getRotationShamt(now.reg(rs2), XLEN))).asUInt }
-    when(ror(inst)) { decodeR; next.reg(rd) := (now.reg(rs1) >> getRotationShamt(now.reg(rs2), XLEN)).asUInt | (now.reg(rs1) << (XLEN.U - getRotationShamt(now.reg(rs2), XLEN))).asUInt }
-    when(rori(inst)){ decodeI; next.reg(rd) := (now.reg(rs1) >> getRotationShamt(imm, XLEN)).asUInt | (now.reg(rs1) << (XLEN.U - getRotationShamt(imm, XLEN))).asUInt }
+    when(rol(inst))  { decodeR; next.reg(rd) := (now.reg(rs1) << getRotationShamt(now.reg(rs2), XLEN)).asUInt | (now.reg(rs1) >> (XLEN.U - getRotationShamt(now.reg(rs2), XLEN))).asUInt }
+    when(ror(inst))  { decodeR; next.reg(rd) := (now.reg(rs1) >> getRotationShamt(now.reg(rs2), XLEN)).asUInt | (now.reg(rs1) << (XLEN.U - getRotationShamt(now.reg(rs2), XLEN))).asUInt }
+    when(rori(inst)) { decodeI; next.reg(rd) := (now.reg(rs1) >> getRotationShamt(imm, XLEN)).asUInt | (now.reg(rs1) << (XLEN.U - getRotationShamt(imm, XLEN))).asUInt }
     // OR Combine
     when(orc_b(inst)) {
       decodeR;
       val byteResults = VecInit(Seq.fill(XLEN / 8)(0.U(8.W)))
       for (i <- 0 until XLEN by 8) {
         val byte = now.reg(rs1)(i + 7, i)
-        byteResults(i / 8) := Mux(byte.orR, 0xFF.U(8.W), 0x00.U(8.W))
+        byteResults(i / 8) := Mux(byte.orR, 0xff.U(8.W), 0x00.U(8.W))
       }
       next.reg(rd) := byteResults.asUInt
     }
@@ -152,20 +154,21 @@ trait BExtension extends BaseCore with CommonDecode with BExtensionInsts {
     when(rev8(inst)) {
       decodeR;
       var result = 0.U(XLEN.W)
-      var j = XLEN - 8
+      var j      = XLEN - 8
       for (i <- 0 until XLEN by 8) {
         result = result | (now.reg(rs1)(j + 7, j) << i).asUInt
         j -= 8
       }
       next.reg(rd) := result
     }
-
+    // scalafmt: { maxColumn = 120 } (back to defaults)
   }
+
   /** Carry-less multiplication
-   * - riscv-spec-20240411 P214
-   * - Chapter 28.4.3
-   * Carry-less multiplication is the multiplication in the polynomial ring over GF(2).
-   */
+    *
+    *   - riscv-spec-20240411 P214
+    *   - 28.4.3. Zbc: Carry-less multiplication
+    */
   def doRV32Zbc: Unit = {
     when(clmul(inst)) {
       decodeR;
@@ -180,7 +183,7 @@ trait BExtension extends BaseCore with CommonDecode with BExtensionInsts {
     when(clmulh(inst)) {
       decodeR;
       val partialResults = VecInit(Seq.fill(XLEN)(0.U(XLEN.W)))
-      for (i <- 1 to  XLEN) {
+      for (i <- 1 to XLEN) {
         when(((now.reg(rs2) >> i.U) & 1.U) > 0.U) {
           partialResults(i - 1) := now.reg(rs1) >> (XLEN - i)
         }
@@ -199,20 +202,20 @@ trait BExtension extends BaseCore with CommonDecode with BExtensionInsts {
     }
 
   }
+
   /** Single-bit instructions
-   * - riscv-spec-20240411 P215
-   * - Chapter 28.4.4
-   * The single-bit instructions provide a mechanism to set, clear, invert, or extract a single bit in a register.
-   * The bit is specified by its index.
-   */
+    *
+    *   - riscv-spec-20240411 P215
+    *   - 28.4.4. Zbs: Single-bit instructions
+    */
   def doRV32Zbs: Unit = {
-    when(bclr(inst)) {}
+    when(bclr(inst))  {}
     when(bclri(inst)) {}
-    when(bext(inst)) {}
+    when(bext(inst))  {}
     when(bexti(inst)) {}
-    when(binv(inst)) {}
+    when(binv(inst))  {}
     when(binvi(inst)) {}
-    when(bset(inst)) {}
+    when(bset(inst))  {}
     when(bseti(inst)) {}
   }
 
@@ -222,13 +225,13 @@ trait BExtension extends BaseCore with CommonDecode with BExtensionInsts {
       decodeR; next.reg(rd) := now.reg(rs2) + zeroExt(now.reg(rs1)(31, 0), XLEN)
     }
     when(sh1add_uw(inst)) {
-      decodeR; next.reg(rd) := now.reg(rs2) + (zeroExt(now.reg(rs1)(31, 0), XLEN)  << 1)
+      decodeR; next.reg(rd) := now.reg(rs2) + (zeroExt(now.reg(rs1)(31, 0), XLEN) << 1)
     }
     when(sh2add_uw(inst)) {
-      decodeR; next.reg(rd) := now.reg(rs2) + (zeroExt(now.reg(rs1)(31, 0), XLEN)  << 2)
+      decodeR; next.reg(rd) := now.reg(rs2) + (zeroExt(now.reg(rs1)(31, 0), XLEN) << 2)
     }
     when(sh3add_uw(inst)) {
-      decodeR; next.reg(rd) := now.reg(rs2) + (zeroExt(now.reg(rs1)(31, 0), XLEN)  << 3)
+      decodeR; next.reg(rd) := now.reg(rs2) + (zeroExt(now.reg(rs1)(31, 0), XLEN) << 3)
     }
     when(slli_uw(inst)) {
       decodeI; next.reg(rd) := zeroExt(now.reg(rs1)(31, 0), XLEN) << imm(5, 0)
@@ -236,6 +239,7 @@ trait BExtension extends BaseCore with CommonDecode with BExtensionInsts {
     // pseudoinstructions: zext.w rd, rs(Add unsigned word)
   }
   def doRV64Zbb(): Unit = {
+    // scalafmt: { maxColumn = 200 }
     doRV32Zbb
     // Count leading/trailing zero bits
     when(clzw(inst)) { decodeI; next.reg(rd) := Mux(now.reg(rs1) === 0.U, 32.U, PriorityEncoder(now.reg(rs1)(31, 0).asBools.reverse)) }
@@ -249,19 +253,19 @@ trait BExtension extends BaseCore with CommonDecode with BExtensionInsts {
     when(rolw(inst)) {
       decodeR;
       val rs1_data = zeroExt(now.reg(rs1)(31, 0), XLEN)
-      val result = ((rs1_data << now.reg(rs2)(4, 0)).asUInt | (rs1_data >> (32.U - now.reg(rs2)(4, 0))).asUInt)
+      val result   = ((rs1_data << now.reg(rs2)(4, 0)).asUInt | (rs1_data >> (32.U - now.reg(rs2)(4, 0))).asUInt)
       next.reg(rd) := signExt(result(31, 0), XLEN)
     }
     when(roriw(inst)) {
       decodeI;
       val rs1_data = zeroExt(now.reg(rs1)(31, 0), XLEN)
-      val result = (rs1_data >> imm(4, 0)).asUInt | (rs1_data << (32.U - imm(4, 0))).asUInt
+      val result   = (rs1_data >> imm(4, 0)).asUInt | (rs1_data << (32.U - imm(4, 0))).asUInt
       next.reg(rd) := signExt(result(31, 0), XLEN)
     }
     when(rorw(inst)) {
       decodeR;
       val rs1_data = zeroExt(now.reg(rs1)(31, 0), XLEN)
-      val result = (rs1_data >> now.reg(rs2)(4, 0)).asUInt | (rs1_data << (32.U - now.reg(rs2)(4, 0))).asUInt
+      val result   = (rs1_data >> now.reg(rs2)(4, 0)).asUInt | (rs1_data << (32.U - now.reg(rs2)(4, 0))).asUInt
       next.reg(rd) := signExt(result(31, 0), XLEN)
     }
 
@@ -269,13 +273,14 @@ trait BExtension extends BaseCore with CommonDecode with BExtensionInsts {
     when(rev8(inst)) {
       decodeR;
       var result = 0.U(XLEN.W)
-      var j = XLEN - 8
+      var j      = XLEN - 8
       for (i <- 0 until XLEN by 8) {
         result = result | (now.reg(rs1)(j + 7, j) << i).asUInt
         j -= 8
       }
       next.reg(rd) := result
     }
+    // scalafmt: { maxColumn = 120 } (back to defaults)
   }
   def doRV64Zbc(): Unit = {
     doRV32Zbc
@@ -287,38 +292,31 @@ trait BExtension extends BaseCore with CommonDecode with BExtensionInsts {
     when(binvi(inst)) {}
     when(bseti(inst)) {}
   }
+
   /** Bit-manipulation for Cryptography
-   * - riscv-spec-20240411 P215
-   * - Chapter 28.4.5
-   * This extension contains instructions essential for implementing common operations in cryptographic
-   * workloads.
-   */
+    *
+    *   - riscv-spec-20240411 P215
+    *   - 28.4.5. Zbkb: Bit-manipulation for Cryptography
+    */
   def doRV32Zbkb: Unit = {
-    when(pack(inst)) {}
+    when(pack(inst))  {}
     when(packh(inst)) {}
     when(rev_b(inst)) {}
 
   }
+
   /** Carry-less multiplication for Cryptography
-   * - riscv-spec-20240411 P216
-   * - Chapter 28.4.6
-   * Carry-less multiplication is the multiplication in the polynomial ring over GF(2). This is a critical
-   * operation in some cryptographic workloads, particularly the AES-GCM authenticated encryption
-   * scheme. This extension provides only the instructions needed to efficiently implement the GHASH
-   * operation, which is part of this workload.
-   */
-  def doRV32Zbkc: Unit = {
-  }
+    *
+    *   - riscv-spec-20240411 P216
+    *   - 28.4.6. Zbkc: Carry-less multiplication for Cryptography
+    */
+  def doRV32Zbkc: Unit = {}
+
   /** Crossbar permutations
-   * - riscv-spec-20240411 P216
-   * - Chapter 28.4.7
-   * These instructions implement a "lookup table" for 4 and 8 bit elements inside the general purpose
-   * registers. rs1 is used as a vector of N-bit words, and rs2 as a vector of N-bit indices into rs1. Elements in
-   * rs1 are replaced by the indexed element in rs2, or zero if the index into rs2 is out of bounds.
-   * These instructions are useful for expressing N-bit to N-bit boolean operations, and implementing
-   * cryptographic code with secret dependent memory accesses (particularly SBoxes) such that the
-   * execution latency does not depend on the (secret) data being operated on.
-   */
+    *
+    *   - riscv-spec-20240411 P216
+    *   - 28.4.7. Zbkx: Crossbar permutations
+    */
   def doRV32Zbkx: Unit = {
     when(xperm_b(inst)) {}
     when(xperm_n(inst)) {}
@@ -327,7 +325,7 @@ trait BExtension extends BaseCore with CommonDecode with BExtensionInsts {
   def doRV64Zbkb(): Unit = {
     doRV32Zbkb
     when(packw(inst)) {}
-    when(zip(inst)) {}
+    when(zip(inst))   {}
     when(unzip(inst)) {}
   }
   def doRV64Zbkc(): Unit = {
@@ -338,31 +336,31 @@ trait BExtension extends BaseCore with CommonDecode with BExtensionInsts {
   }
 
   def doRV32B: Unit = {
-    if(config.extensions.Zba){
+    if (config.extensions.Zba) {
       doRV32Zba
     }
-    if(config.extensions.Zbb){
+    if (config.extensions.Zbb) {
       doRV32Zbb
     }
-    if(config.extensions.Zbc){
+    if (config.extensions.Zbc) {
       doRV32Zbc
     }
-    if(config.extensions.Zbs){
+    if (config.extensions.Zbs) {
       doRV32Zbs
     }
   }
   def doRV64B: Unit = {
     doRV32B
-    if(config.extensions.Zba){
+    if (config.extensions.Zba) {
       doRV64Zba
     }
-    if(config.extensions.Zbb){
+    if (config.extensions.Zbb) {
       doRV64Zbb
     }
-    if(config.extensions.Zbc){
+    if (config.extensions.Zbc) {
       doRV64Zbc
     }
-    if(config.extensions.Zbs){
+    if (config.extensions.Zbs) {
       doRV64Zbs
     }
   }
@@ -373,5 +371,3 @@ trait BExtension extends BaseCore with CommonDecode with BExtensionInsts {
     }
   }
 }
-
-// scalafmt: { maxColumn = 120 } (back to defaults)
